c4a45bcd35c42be4183911c81481d93aedb432ef
Andrew Kreps <andrewkreps@lululemon.com>
2017-11-01 00:48:53 -0700

    Give it a whirl.

---
+++ lib/lita/handlers/onewheel_loggly.rb
@@ -0,0 +1,84 @@
+require 'json'
+require 'rest-client'
+
+module Lita
+  module Handlers
+    class OnewheelLoggly < Handler
+      config :api_key, required: true
+      config :base_uri, required: true
+      config :query, required: true
+
+      route /^logs\s*(\w+)$/i, :logs, command: true
+
+      def logs(response)
+        auth_header = {'Authorization': "bearer #{config.api_key}"}
+
+        #   last_10_events_query = "/iterate?q=*&from=-10m&until=now&size=10"
+
+        from_time = '-10m'
+        if response.matches[0][0]
+          from_time = response.matches[0][0]
+          unless from_time[0] == '-' from_time = "-#{from_time}"
+        end
+
+        sample_query = "/iterate?q=#{CGI::escape config.query}&from=#{from_time}&until="
+        uri = "#{config.base_uri}#{sample_query}"
+        Lita.logger.debug uri
+        resp = RestClient.get uri, auth_header
+
+        alerts = {}
+
+        events = JSON.parse resp.body
+
+        events['events'].each do |event|
+          msg = JSON.parse event['logmsg']
+          message = msg['message']
+          if md = /(fault=[a-zA-Z0-9.-]+)/.match(message)
+            alerts[md[0]] = 0 unless alerts[md[0]]
+            alerts[md[0]] += 1
+          elsif /KeyError/.match(message)
+            idx = message.index('KeyError')
+            salient = message[idx - 120, 150]
+            salient.gsub! /\\n - \w+/, ''
+            alerts[salient] = 0 unless alerts[salient]
+            alerts[salient] += 1
+          elsif /IndexError/.match(message)
+            idx = message.index('IndexError')
+            salient = message[idx - 120, 150]
+            alerts[salient] = 0 unless alerts[salient]
+            alerts[salient] += 1
+          elsif md = /(socket\.timeout: The read operation timed out)/.match(message)
+            salient = "Unhandled #{md[0]}"
+            alerts[salient] = 0 unless alerts[salient]
+            alerts[salient] += 1
+          elsif md = /(urllib3\.exceptions\.ProtocolError: \('Connection aborted\.', ConnectionResetError\(104, 'Connection reset by peer'\)\))/.match(message)
+            salient = "Unhandled #{md[0]}"
+            alerts[salient] = 0 unless alerts[salient]
+            alerts[salient] += 1
+          elsif md = /(http\.client\.RemoteDisconnected: Remote end closed connection without response)/.match(message)
+            salient = "Unhandled #{md[0]}"
+            alerts[salient] = 0 unless alerts[salient]
+            alerts[salient] += 1
+          else
+            salient = message.gsub /\\n/, "\n"
+            Lita.logger.debug salient
+            salient = 'unknown'
+            alerts[salient] = 0 unless alerts[salient]
+            alerts[salient] += 1
+          end
+        end
+
+        Lita.logger.debug "#{events['events'].count} events"
+        response.reply "#{events['events'].count} events"
+
+        alerts.each do |key, count|
+          Lita.logger.debug "Counted #{count}: #{key}"
+          response.reply "Counted #{count}: #{key}"
+        end
+
+      end
+
+      Lita.register_handler(self)
+    end
+  end
+end
---
+++ spec/lita/handlers/onewheel_loggly_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'onewheel-google'
+
+def mock_it_up(file)
+  mock_result_json = File.open("spec/fixtures/#{file}.json").read
+  allow(OnewheelGoogle).to receive(:search).and_return(JSON.parse mock_result_json)
+end
+
+describe Lita::Handlers::OnewheelLoggly, lita_handler: true do
+
+  before(:each) do
+    registry.configure do |config|
+      # config.handlers.onewheel_images.custom_search_engine_id = ''
+      # config.handlers.onewheel_images.google_api_key = ''
+    end
+  end
+
+  it { is_expected.to route_command('logs -10m') }
+
+  it 'does neat imagey things' do
+    mock_it_up('mock_result')
+
+    send_command 'image yo'
+    expect(replies.last).to eq('https://s-media-cache-ak0.pinimg.com/736x/4a/43/a4/4a43a4b6569cf8a197b6c9217de3f412.jpg')
+  end
+
+  it 'does neat gif-y things' do
+    mock_it_up('giphy_result')
+    send_command 'giphy boop'
+    expect(replies.last).to eq('https://media.giphy.com/media/lcvjDNIJ8CS88/giphy.gif')
+  end
+
+  it 'does neat gif-y things' do
+    mock_it_up('giphy_tophat_result')
+    send_command 'giphy tophat'
+    expect(replies.last).to eq('https://media4.giphy.com/media/7fLGzTYkX3hO8/giphy.gif')
+  end
+end
